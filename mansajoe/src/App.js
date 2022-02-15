4d09ef5b142cd33b9b86cc532eb897bf334f29b5
Joseph King <joeykng2@gmail.com>
2022-02-15 07:51:54 -0500

    Working on the profile part of the resume

--- mansajoe/src/App.js
+++ mansajoe/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
 import NavBar from './Components/NavBar/NavBar'
+import Profile from './Components/Profile/Profile';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Profile />
     </div>
   );
 }
